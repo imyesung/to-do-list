09d4b5f497db5d07a2648f0f63bf6cdbd5f98caa
Yesung <61532423+after-kubrick@users.noreply.github.com>
2021-08-05 13:17:44 +0900

    Add login page
    -Prevent default events

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const loginForm = document.queryCommandEnabled("login-form");
+const loginForm = document. querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
-const loginButton = document.querySelector(".login-form button");
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);}
+
+loginForm.addEventListener("submit", onLoginSubmit);

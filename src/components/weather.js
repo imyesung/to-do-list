f6199a0da8f057f3bdd2424d39c92144092d59a5
Lim Yesung <after.kubrick@gmail.com>
2021-08-14 17:53:05 +0900

    🚧 Move Paths

---
+++ src/components/weather.js
@@ -0,0 +1,18 @@
+const API_KEY = "74cd9157b0dd1fda3186c46c2025bd39";
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;})
+}
+function onGeoError() {
+    alert("Can't find you :(")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
